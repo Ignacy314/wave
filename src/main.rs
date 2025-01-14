0d55ef30fecf1077ac7d887136b7302663b151d8
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-01-14 16:00:49 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use std::env;
-use std::fs::File;
-use std::io::Read;
 use std::mem::transmute;
 
 use chrono::DateTime;
@@ -11,7 +9,8 @@ fn main() {
     println!("{:?}", reader.spec());
     let mut pps = false;
     let mut first_read = false;
-    let mut nanos = 0i64;
+    //let mut nanos = 0i64;
+    let mut prev = 0i32;
     for s in reader.samples::<i32>() {
         let sample = s.unwrap();
         #[allow(clippy::cast_possible_wrap)]
@@ -21,45 +20,13 @@ fn main() {
             if first_read {
                 pps = false;
                 first_read = false;
-                nanos = unsafe { transmute::<[i32; 2], i64>([sample, (nanos >> 32) as i32]) };
+                let nanos = unsafe { transmute::<[i32; 2], i64>([sample, prev]) };
                 let dt = DateTime::from_timestamp_nanos(nanos);
                 eprintln!("{dt}");
             } else {
                 first_read = true;
-                nanos = i64::from(sample) << 32;
+                prev = sample;
             }
         }
     }
-    //let mut file = File::open(args[1].clone()).unwrap();
-    //let mut buf = [0u8; 4];
-    //let mut pps = false;
-    //let mut first_read = false;
-    //let mut nanos = 0i64;
-    //while let Ok(s) = file.read(&mut buf) {
-    //    if s == 0 {
-    //        break;
-    //    }
-    //
-    //    let sample: i32 = i32::from_ne_bytes(buf);
-    //    #[allow(clippy::cast_possible_wrap)]
-    //    if sample == 0xeeee_eeee_u32 as i32 {
-    //        pps = true;
-    //    } else if pps {
-    //        println!("{sample:#x}");
-    //        if first_read {
-    //            pps = false;
-    //            first_read = false;
-    //            println!("{nanos:#x}");
-    //            println!("{:#x}", i64::from(sample) & 0x1111_1111);
-    //            nanos = unsafe { transmute([sample, (nanos >> 32) as i32])};
-    //            //nanos += i64::from(sample) & 0x1111_1111;
-    //            println!("{nanos:#x}");
-    //            let dt = DateTime::from_timestamp_nanos(nanos);
-    //            eprintln!("{dt}");
-    //        } else {
-    //            first_read = true;
-    //            nanos = i64::from(sample) << 32;
-    //        }
-    //    }
-    //}
 }
