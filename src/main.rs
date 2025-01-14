29eb3319ec47f3c374cb026a8f2ae262e18bdd8e
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-01-14 15:42:13 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,7 @@ fn main() {
                 pps = false;
                 first_read = false;
                 println!("{nanos:#x}");
+                println!("{:#x}", i64::from(sample));
                 nanos += i64::from(sample);
                 println!("{nanos:#x}");
                 let dt = DateTime::from_timestamp_nanos(nanos);
