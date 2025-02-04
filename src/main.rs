3fb164c660606906365dc99429bd0eb99e48213f
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-02-04 22:33:21 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 //#![allow(unused)]
 use clap::Parser;
-use std::env;
 
 use chrono::DateTime;
 
@@ -10,22 +9,22 @@ mod umc;
 
 #[derive(Parser, Debug)]
 struct Args {
-    // Start timestamp in format "%Y-%m-%d %H:%M:%S%.3f %z"
+    /// Start timestamp in format "%Y-%m-%d %H:%M:%S%.3f %z"
     start_time: String,
-    // End timestamp in the same format as start timestamp
+    /// End timestamp in the same format as start timestamp
     end_time: String,
-    // Path to output file (without .wav extension - for i2s mode it's the base name for all output files)
+    /// Path to output file (without .wav extension - for i2s mode it's the base name for all output files)
     output: String,
-    // Path to input directory containing wav files with names being numbers of nanoseconds since unix epoch
+    /// Path to input directory containing wav files with names being numbers of nanoseconds since unix epoch
     input_dir: String,
-    // 'umc' or 'i2s'
+    /// 'umc' or 'i2s'
     mode: String,
     #[arg(short, long)]
-    // Path to directory containing csv files describing drone flights
+    /// Path to directory containing csv files describing drone flights
     csv_dir: Option<String>,
     #[arg(short, long)]
-    // Path to file containing in each row a single timestamp (nanoseconds since unix epoch) of an
-    // alsa error
+    /// Path to file containing in each row a single timestamp (nanoseconds since unix epoch) of an
+    /// alsa error
     errors_file: Option<String>
 }
 
