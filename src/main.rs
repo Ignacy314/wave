6ed60dc46f4b1b12673d04b5c5674551cfb9af5d
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-03-02 18:24:05 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,161 @@
-//#![allow(unused)]
-use clap::{Parser, Subcommand};
+use std::path::Path;
 
-use chrono::DateTime;
+use clap::Parser;
 
-use self::concat::concat;
-
-mod concat;
-mod i2s;
-mod pps;
-mod umc;
+use chrono::{DateTime, FixedOffset};
+use serde::Deserialize;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-#[command(propagate_version = true)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Concatenates all wav files in a folder
-    Concat(ConcatArgs),
-    /// Concat and optionally cut waves while removing PPS
-    Cut(Args),
-}
-
-#[derive(clap::Args)]
-struct ConcatArgs {
-    #[arg(short, long)]
-    /// Path to output file
-    output: String,
-    #[arg(short, long)]
-    /// Path to input directory containing wav files
-    input_dir: String,
-}
-
-#[derive(clap::Args)]
 struct Args {
-    #[clap(flatten)]
-    time_or_csv: TimeOrCsv,
+    /// Start timestamp
     #[arg(short, long)]
-    /// Path to output file (without .wav extension - for i2s mode it's the base name for all output files)
+    start: Option<String>,
+    /// Path to output dir plus file name stem
+    #[arg(short, long)]
     output: String,
+    /// Path to input wav file
     #[arg(short, long)]
-    /// Path to input directory containing wav files with names being numbers of nanoseconds since unix epoch
-    input_dir: String,
+    input: String,
+    /// Path to csv with split timestamps
     #[arg(short, long)]
-    /// 'umc' or 'i2s'
-    mode: String,
-    #[arg(short, long)]
-    /// Path to file containing in each row a single timestamp (nanoseconds since unix epoch) of an
-    /// alsa error
-    errors_file: Option<String>,
+    csv: String,
 }
 
-#[derive(Debug, clap::Args)]
-#[group(required = true, multiple = false)]
-struct TimeOrCsv {
-    #[arg(
-        short,
-        long,
-        value_delimiter = ' ',
-        num_args = 2,
-        value_name = "TIMESTAMP"
-    )]
-    /// Timestamps of start and end in the rfc3339 format delimited by a whitespace
-    timestamps: Option<Vec<String>>,
-    #[arg(short, long)]
-    /// Path to directory containing csv files describing drone flights
-    csv_file: Option<String>,
+#[derive(Deserialize)]
+struct SplitRecord {
+    start: String,
+    stop: String,
+}
+
+struct Split {
+    start: DateTime<FixedOffset>,
+    stop: DateTime<FixedOffset>,
+}
+
+fn time_diff_to_samples(start: DateTime<FixedOffset>, stop: DateTime<FixedOffset>) -> u32 {
+    assert!(stop >= start);
+    let diff = stop - start;
+    let nanos = diff.num_nanoseconds().unwrap();
+    (nanos as f64 / 1e9_f64 * 48000.0).round() as u32
 }
 
 fn main() {
-    //let args = Args::parse();
-    let cli = Cli::parse();
+    let args = Args::parse();
 
-    match cli.command {
-        Commands::Cut(args) => {
-            let timestamps = if let Some(timestamps) = args.time_or_csv.timestamps {
-                //let from = DateTime::parse_from_str(&timestamps[0], "%Y-%m-%d %H:%M:%S%.3f %z").unwrap();
-                //let to = DateTime::parse_from_str(&timestamps[1], "%Y-%m-%d %H:%M:%S%.3f %z").unwrap();
-                let from = DateTime::parse_from_rfc3339(&timestamps[0]).unwrap();
-                let to = DateTime::parse_from_rfc3339(&timestamps[1]).unwrap();
-                Some((from, to))
+    let start = if let Some(start) = args.start {
+        DateTime::parse_from_rfc3339(&start).unwrap()
+    } else {
+        let path = Path::new(&args.input);
+        let filename = path.file_stem().unwrap().to_str().unwrap();
+        let split_index = filename.match_indices('_').last().unwrap();
+        let timestamp = &filename[split_index.0..];
+        DateTime::parse_from_rfc3339(timestamp).unwrap()
+    };
+
+    let mut reader = hound::WavReader::open(args.input).unwrap();
+
+    let mut csv = csv::Reader::from_path(args.csv).unwrap();
+    let records = csv.deserialize();
+
+    let mut splits = Vec::new();
+    for record in records {
+        let split_record: SplitRecord = record.unwrap();
+        let split = Split {
+            start: DateTime::parse_from_rfc3339(&split_record.start).unwrap(),
+            stop: DateTime::parse_from_rfc3339(&split_record.stop).unwrap(),
+        };
+        splits.push(split);
+    }
+
+    let mut splits_iter = splits.iter();
+    let mut next_split = splits_iter.next().unwrap();
+
+    let mut write = start < next_split.start;
+    let mut samples = if write {
+        time_diff_to_samples(start, next_split.start)
+    } else {
+        time_diff_to_samples(next_split.start, next_split.stop)
+    };
+
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 48000,
+        bits_per_sample: 32,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+    let mut i = 0;
+    let mut write_until_finish = false;
+
+    let mut writer =
+        Some(hound::WavWriter::create(format!("{}_{i}.wav", args.output), spec).unwrap());
+
+    for sample in reader.samples::<i32>().step_by(2) {
+        let s = sample.unwrap();
+
+        if !write_until_finish && samples == 0 {
+            if write {
+                samples = time_diff_to_samples(next_split.start, next_split.stop);
+                write = false;
+                writer.unwrap().finalize().unwrap();
+                writer = None;
             } else {
-                None
-            };
-
-            let output = args.output;
-            let input_dir = args.input_dir;
-
-            let mode = args.mode;
-            if mode == "umc" {
-                umc::make_wav(
-                    timestamps,
-                    output,
-                    input_dir,
-                    args.time_or_csv.csv_file,
-                    args.errors_file,
+                let start = next_split.stop;
+                if let Some(next) = splits_iter.next() {
+                    next_split = next;
+                    samples = time_diff_to_samples(start, next_split.start);
+                } else {
+                    write_until_finish = true;
+                }
+                write = true;
+                i += 1;
+                writer = Some(
+                    hound::WavWriter::create(format!("{}_{i}.wav", args.output), spec).unwrap(),
                 );
-            } else if mode == "i2s" {
-                i2s::make_wav(timestamps, output, input_dir);
-            } else if mode == "concat" {
-            } else {
-                eprintln!("Mode can be 'umc', 'i2s' or 'concat'");
             }
         }
-        Commands::Concat(args) => {
-            concat(args.input_dir, args.output);
+
+        if write {
+            if let Some(writer) = writer.as_mut() {
+                writer.write_sample(s).unwrap();
+            }
         }
+        samples -= 1;
     }
+
+    //match cli.command {
+    //    Commands::Cut(args) => {
+    //        let timestamps = if let Some(timestamps) = args.time_or_csv.timestamps {
+    //            //let from = DateTime::parse_from_str(&timestamps[0], "%Y-%m-%d %H:%M:%S%.3f %z").unwrap();
+    //            //let to = DateTime::parse_from_str(&timestamps[1], "%Y-%m-%d %H:%M:%S%.3f %z").unwrap();
+    //            let from = DateTime::parse_from_rfc3339(&timestamps[0]).unwrap();
+    //            let to = DateTime::parse_from_rfc3339(&timestamps[1]).unwrap();
+    //            Some((from, to))
+    //        } else {
+    //            None
+    //        };
+    //
+    //        let output = args.output;
+    //        let input_dir = args.input_dir;
+    //
+    //        let mode = args.mode;
+    //        if mode == "umc" {
+    //            umc::make_wav(
+    //                timestamps,
+    //                output,
+    //                input_dir,
+    //                args.time_or_csv.csv_file,
+    //                args.errors_file,
+    //            );
+    //        } else if mode == "i2s" {
+    //            i2s::make_wav(timestamps, output, input_dir);
+    //        } else if mode == "concat" {
+    //        } else {
+    //            eprintln!("Mode can be 'umc', 'i2s' or 'concat'");
+    //        }
+    //    }
+    //    Commands::Concat(args) => {
+    //        concat(args.input_dir, args.output);
+    //    }
+    //}
 }
